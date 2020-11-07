ff26c5466fb2b51ea5c820e3c661a27d97a2f19c
Daniel Sacramento <danisacra@gmail.com>
2020-11-07 17:58:27 -0300

    Added model Game

---
+++ spec/factories/products.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :product do
+    price { Faker::Commerce.price(range: 100.0..400.0) }
+    
+    after :build do |product|
+      product.productable = create(:game)
+    end
+  end
+end
