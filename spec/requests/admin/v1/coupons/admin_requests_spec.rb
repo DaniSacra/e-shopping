13900e3d1519d2fd1d33947c5b931a639785ef5c
Daniel Sacramento <danisacra@gmail.com>
2020-11-19 16:10:05 -0300

    Added system_requirements controllers and tests

--- spec/requests/admin/v1/coupons/admin_requests_spec.rb
+++ spec/requests/admin/v1/coupons/admin_requests_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe '', type: :request do
+RSpec.describe 'Admin::V1::Coupons as :admin', type: :request do
   let(:user) { create :user }
 
   context 'GET /coupons' do
