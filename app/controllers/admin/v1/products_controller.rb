526c9fd2112593810c556bcedf419f3e81232679
Daniel Sacramento <danisacra@gmail.com>
2020-11-30 18:37:14 -0300

    Added products and games json

--- app/controllers/admin/v1/products_controller.rb
+++ app/controllers/admin/v1/products_controller.rb
@@ -1,25 +1,30 @@
 module Admin::V1
   class ProductsController < ApiController
-    before_action :load_product, only: %i(update destroy)
-    
+    before_action :load_product, only: %i(show update destroy)
+
     def index
       @products = load_products
     end
 
+    def show; end
+
     def create
-      @product = Product.new
-      @product.attributes = product_params
+      run_service
+    rescue Admin::ProductSavingService::NotSavedProductError
+      render_error(fields: @saving_service.errors)
     end
-
+    
     def update
-      @product.attributes = product_params
-      save_product!
+      run_service
+    rescue Admin::ProductSavingService::NotSavedProductError
+      render_error(fields: @saving_service.errors)
     end
-
+    
     def destroy
+      @product.productable.destroy!
       @product.destroy!
-    rescue
-      render_error(fields: @product.errors.messages)
+    rescue ActiveRecord::RecordNotDestroyed
+      render_error(fields: @product.errors.messages.merge(@product.productable.errors.messages))
     end
 
     private
@@ -28,18 +33,12 @@ module Admin::V1
       @product = Product.find(params[:id])
     end
 
-    def save_product!
-      @product.save!
-      render :show
-    rescue
-      render_error(fields: @product.errors.messages)
-    end
-
     def load_products
-      Product.all
+      permitted = params.permit({ search: :name }, { order: {} }, :page, :length)
+      Admin::ModelLoadingService.new(Product.all, permitted).call
     end
 
-    def run_service
+    def run_service(product = nil)
       @saving_service = Admin::ProductSavingService.new(product_params.to_h, @product)
       @saving_service.call
       @product = @saving_service.product
@@ -48,17 +47,18 @@ module Admin::V1
 
     def product_params
       return {} unless params.has_key?(:product)
-      permitted_params = params.require(:product).permit(:id, :name, :description, :image, :price, :productable)
+      permitted_params = params.require(:product).permit(:id, :name, :description, :image, 
+                                                         :price, :productable,
+                                                         :status, category_ids: [])
       permitted_params.merge(productable_params)
     end
-
+    
     def productable_params
       productable_type = params[:product][:productable] || @product&.productable_type&.underscore
       return unless productable_type.present?
-      productable_attributes = send("#{productable_type}_params")
-      { productable_attributes: productable_attributes }
+      send("#{productable_type}_params")
     end
-
+    
     def game_params
       params.require(:product).permit(:mode, :release_date, :developer, :system_requirement_id)
     end
