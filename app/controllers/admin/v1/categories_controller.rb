876bf14d6a093545ddd519f256ec4aff63469f98
Daniel Sacramento <danisacra@gmail.com>
2020-12-04 15:55:10 -0300

    Recfcatoring paginatin modeling

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -1,51 +1,54 @@
-module Admin::V1
-  class CategoriesController < ApiController
-    before_action :load_category, only: [:update, :show, :destroy]
-    
-    def index
-      @loading_service = Admin::ModelLoadingService.new(Category.all, searchable_params)
-      @loading_service.call
-    end
+module Admin
+  module V1
+    class CategoriesController < ApiController
+      before_action :load_category, only: [:update, :destroy, :show]
 
-    def create
-      @category = Category.new
-      @category.attributes = category_params
-      save_category!
-    end
+      def index
+        @loading_service = Admin::ModelLoadingService.new(Category.all, searchable_params)
+        @loading_service.call
+      end
 
-    def show; end
+      def create
+        @category = Category.new
+        @category.attributes = category_params
+        save_category!
+      end
 
-    def update
-      @category.attributes = category_params
-      save_category!
-    end
+      def show; end
 
-    def destroy
-      @category.destroy!
-    rescue
-      render_error(fields: @category.errors.messages)
-    end
+      def update
+        @category.attributes = category_params
+        save_category!
+      end
 
-    private
+      def destroy
+        @category.destroy!
+      rescue
+        render_error(fields: @category.errors.messages)
+      end
 
-    def load_category
-      @category = Category.find(params[:id])
-    end
+      private
 
-    def searchable_params
-      params.permit({ search: :name }, { order: {} }, :page, :length)
-    end
+      def searchable_params
+        params.permit({ search: :name }, { order: [] }, :page, :length)
+      end
 
-    def category_params
-      return {} unless params.has_key?(:category)
-      params.require(:category).permit(:id, :name)
-    end
+      def load_category
+        @category = Category.find(params[:id])
+      end
 
-    def save_category!
-      @category.save!
-      render :show
-    rescue
-      render_error(fields: @category.errors.messages)
+      def category_params
+        return unless params.key?(:category)
+
+        params.require(:category).permit(:name)
+      end
+
+      def save_category!
+        @category.save!
+        render :show
+      rescue
+        render_error(fields: @category.errors.messages)
+      end
     end
   end
 end
--- app/services/admin/model_loading_service.rb
+++ app/services/admin/model_loading_service.rb
@@ -1,9 +1,9 @@
-module Admin  
+module Admin
   class ModelLoadingService
     attr_reader :records, :pagination
-    
-    def initialize(searchable_model, params = {})
-      @searchable_model = searchable_model
+
+    def initialize(searcheable_model, params = {})
+      @searcheable_model = searcheable_model
       @params = params || {}
       @records = []
       @pagination = { page: @params[:page].to_i, length: @params[:length].to_i }
@@ -11,19 +11,19 @@ module Admin
 
     def call
       fix_pagination_values
-      filtered = @searchable_model.search_by_name(@params.dig(:search, :name))  
+      filtered = @searcheable_model.search_by_name(@params.dig(:search, :name))
+      @records = filtered.order(@params[:order].to_h)
+                         .paginate(@pagination[:page], @pagination[:length])
+
+      total_pages = (filtered.count / @pagination[:length].to_f).ceil
+      @pagination.merge!(total: filtered.count, total_pages: total_pages)
     end
 
     private
 
     def fix_pagination_values
-      @pagination[:page] = @searchable_model.model::DEFAULT_PAGE if @pagination[:page] <= 0
-      @pagination[:length] = @searchable_model.model::MAX_PER_PAGE if @pagination[:length] <= 0
-      @records = filtered.order(@params[:order].to_h).paginate(@pagination[:page], 
-        @pagination[:length])
-      
-      total_pages = (filtered.count / @pagination[:length].to_f).ceil
-      @pagination.merge!(total: filtered.count, total_pages: total_pages)
+      @pagination[:page] = @searcheable_model.model::DEFAULT_PAGE if @pagination[:page] <= 0
+      @pagination[:length] = @searcheable_model.model::MAX_PER_PAGE if @pagination[:length] <= 0
     end
   end
 end
